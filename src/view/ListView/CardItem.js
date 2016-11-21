555eaf1fbe12ee8110e27acd69b86136270f33c5
Tilman Schlenker <tilmans@users.noreply.github.com>
2016-11-21 20:52:56 +0100

    Make cards accept any link

--- src/view/ListView/CardItem.js
+++ src/view/ListView/CardItem.js
@@ -5,7 +5,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import TrelloActions from '../../controller/TrelloActions.js';
-import {getDropZone} from '../../helpers/drop.js';
 
 const CardItem = React.createClass({
 
@@ -20,12 +19,13 @@ const CardItem = React.createClass({
     },
 
     componentDidMount() {
-        this.dropZone = getDropZone(ReactDom.findDOMNode(this));
+        /*this.dropZone = getDropZone(ReactDom.findDOMNode(this));
 
         this.dropZone.evs.drop.on((event) => {
             let deepLink = this.getDeepLink(event);
             TrelloActions.onDrop(this.props.boardId, this.props.card.id, deepLink);
         });
+        */
     },
 
     componentWillUnmount() {
@@ -35,11 +35,22 @@ const CardItem = React.createClass({
     getDeepLink(event) {
         return event.commonObjects[0].deepLink();
     },
+    
+    handleDrop(ev) {
+        ev.preventDefault();
+        var link = ev.dataTransfer.getData("text/uri-list");
+        console.info("Adding link for "+link);
+        TrelloActions.onDrop(this.props.boardId, this.props.card.id, link);
+    },
+    
+    handleDragover(ev) {
+        ev.preventDefault();        
+    },
 
     render() {
         return (
             <div className="trello-plugin-card-item" onClick={this.handleClick}>
-                <div className="trello-plugin-card-drop-target">
+                <div onDrop={this.handleDrop} onDragOver={this.handleDragover} className="trello-plugin-card-drop-target">
                     <span className="trello-plugin-card-item-text">{this.props.card.name}</span>
                     {this.props.card.badges.attachments > 0 ? <span className="trello-plugin-attachments-count">
                         {'Attachments: ' + this.props.card.badges.attachments}
